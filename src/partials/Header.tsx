b7eecf273cba70a42259fa5074179b8ddd87a52f
arvaldez <annarachel.valdez@gmail.com>
2023-03-07 17:31:11 -0600

    put user menu on right side

--- src/partials/Header.tsx
+++ src/partials/Header.tsx
@@ -19,6 +19,10 @@ const Header: React.FC<IHeaderProps> = ({
     <header className="sticky top-0 bg-slate-900 border-b  z-30">
       <div className="px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16 -mb-px">
+          {/* Header: Left side */}
+          <div className="flex"></div>
+
+          {/* Header: Right side */}
           <div className="flex items-center">
             <hr className="w-px h-6 bg-slate-200 mx-3" />
             <UserMenu />
